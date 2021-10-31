d3d5639bf6ba050f9ff15b86a8a5e39e25cad552
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-10-31 12:27:00 -0500

    5

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-const express = require('express')
+const express = require('express');
 const { MongoClient } = require('mongodb');
-require('dotenv').config()
+const bodyParser = require("body-parser");
+// const { MongoClient } = require('mongodb').MongoClient;
+// const ObjectId = require('ObjectId').ObjectId;
 
-const app = express()
-const port = process.env.PORT || 7000;
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jvpmw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-
-console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-
-async function run(){
-    try{
-        // await client.connect();
-        console.log('database connected')
-    }
-    finally{
-        // await client.close()
-    }
-
-}
-run().catch(console.dir);
+// middlewere
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello tourist')
+});
+client.connect((err)=>{
+    const productCollection= client.db("foodShop").collection("products");
+    const orderCollection=client.db("foodShop").collection("orders");
+
+      // perform actions on the collection object
+  client.close();
+
+
+    app.post("/order",(req, res)=>{
+        productCollection.insertOne(req.body).then((result)=>{
+            res.send(result.insertedId);
+        //    console.log(result);
+       });
+});
+
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening tawfiq tawfiq http://localhost:${port}`)
