9f66b871d32da78a4685826d659390a9c3cb0490
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-10-30 23:02:31 -0500

    2

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
 const app = express()
-const port = 3000
+const port = 7000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jvpmw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+async function run(){
+    try{
+        // await client.connect();
+        console.log('database connected')
+    }
+    finally{
+        // await client.close()
+    }
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
